dec91ebfb3b740bff40a237cd6391be95953bd4f
Warodom WERAPUN <wwarodom@gmail.com>
2025-03-17 09:51:30 +0700

    add task

--- my-app/app/todo2/page.tsx
+++ my-app/app/todo2/page.tsx
@@ -10,11 +10,25 @@ const STYLE = 'border-2 border-black px-2'
 
 
 export default function Todo2() {
+
+    const [formTask, setFormTask] = useState<TaskType>({id: -1, name: '', time:0});
+
     const [tasks, setTasks] = useState<TaskType[]>([
         { id: 1, name: "Reading a book", time: 100 },
         { id: 2, name: "Writing a program", time: 30 },
         { id: 3, name: "Sleeping", time: 120 },
     ]);
+
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        const {name, value} = e.target; 
+        setFormTask({...formTask, [name]: value});
+    }
+
+    function add() {
+        const newTask = { ...formTask, id: tasks.length + 1};
+        setTasks([...tasks, newTask]);
+        setFormTask({id: -1, name: '', time: 0});
+    }
 
     return (
         <div>
@@ -25,16 +39,24 @@ export default function Todo2() {
                     <label htmlFor="name">Name: </label>
                     <input 
                         className={STYLE}
-                        type="text" name="name" />
+                        type="text" name="name"
+                        value={formTask.name} 
+                        onChange={handleInput}
+                        />
                 </div>
                 <div className="mb-2">
                     <label htmlFor="name">Time: </label>
                     <input 
                         className={STYLE}
-                        type="number" name="time" />
+                        type="number" name="time" 
+                        value={formTask.time} 
+                        onChange={handleInput}
+                        />
                 </div>
                 <div>
-                    <button className={STYLE}>Add</button>
+                    <button className={STYLE}
+                        onClick={add}
+                    >Add</button>
                 </div>
 
             </div>
