a491f89dba2f31bee7c0e6f3ef142e056578e118
Warodom WERAPUN <wwarodom@gmail.com>
2025-03-24 10:32:14 +0700

    add task

--- my-app/app/simple_db/page.tsx
+++ my-app/app/simple_db/page.tsx
@@ -1,4 +1,7 @@
 import prisma from "@/utils/db"
+import { revalidatePath } from "next/cache"
+
+const STYLE='border-2 border-black m-2 p-2'
 
 export default async function SimpleDb() {
 
@@ -9,25 +12,49 @@ export default async function SimpleDb() {
   //   }
   // })
 
-  const todo = await prisma.todo.findFirst()
-  console.log("First record: ", todo)
+  // const todo = await prisma.todo.findFirst()
+  // console.log("First record: ", todo)
+
+  // const todoX = await prisma.todo.findMany( { where: { title: "xxx" } } )
+  // console.log("X record: ", todoX)
 
   const todos = await prisma.todo.findMany()
-  console.log("First record: ", todos)
 
-  const todoX = await prisma.todo.findMany( { where: { title: "xxx" } } )
-  console.log("X record: ", todoX)
+  async function addTask(formData: FormData) {
+    "use server"
+    const title = formData.get("title") as string
+    const done = formData.get("done") === "on"
+    console.log("title: ", title, "done: ", done? "true" : "false")
+    await prisma.todo.create({  
+      data: {
+        title,
+        done,
+      }
+    })
+    revalidatePath("/simple_db")
+  }
 
   return (
     <div>
       <h1>Simple DB</h1>
-      find first : {JSON.stringify(todo)}
-      <hr />
-      <br />
-      find many : { JSON.stringify(todos) }
-      <hr />
-      <br />
-      find with condition todoX: { JSON.stringify(todoX) }
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={todo.id}>
+            {index + 1}. {todo.title} {todo.done ? "✅" : "❌"}
+          </li>
+        ))
+        }
+      </ul>
+
+      <div>
+        <h2>Add task</h2>
+        <form action={addTask}>
+          <input className={STYLE} type="text" name="title" placeholder="Title" />
+          <input className={STYLE} type="checkbox" name="done" />
+          <button className={STYLE} type="submit">Add</button>
+        </form>
+      </div>
+
     </div>
   )
 }
