c43462910347e8f847e84df0afff2261aeae99f6
Warodom WERAPUN <wwarodom@gmail.com>
2025-03-31 09:40:38 +0700

    error handling

--- my-app/app/simple_db/_actions/registerUser.ts
+++ my-app/app/simple_db/_actions/registerUser.ts
@@ -1,18 +1,27 @@
 "use server"
 
 import prisma from "@/utils/db"
+import hashPassword from "@/utils/hashPassword"
 
-export default async function registerUser(formData: FormData) { 
+export default async function registerUser(prevState: unknown ,formData: FormData) { 
     const name = formData.get("name") as string
     const email = formData.get("email") as string
-    const password = formData.get("password") as string
+    const password =  await hashPassword(formData.get("password") as string)
     console.log(name, email, password) 
 
-    await prisma.user.create({
-        data: {
-            name:name,
-            email,
-            password
+    try {
+        await prisma.user.create({data: { name, email, password}})
+    }
+    catch(e) {
+        // console.log(e)
+        return {
+            error: "Error: " + e,
+            message: ""
         }
-    })
+    }
+    return {
+        error: "",
+        message: "User registered successfully"
+    }
+    
 }
--- my-app/app/simple_db/register/page.tsx
+++ my-app/app/simple_db/register/page.tsx
@@ -1,38 +1,55 @@
-import STYLE from "@/constants/style"; 
+"use client"
+
+import STYLE from "@/constants/style";
 import registerUser from "../_actions/registerUser";
+import { useActionState } from "react";
 
 export default function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <div>
-        <form action={registerUser}>
-            <div>
-                <label htmlFor="name">Name: </label>
-                <input
-                    className={STYLE}
-                    name="name"
-                    type="text" maxLength={20} required />
+
+    const [state, action] = useActionState(registerUser, { error: "", message: "" })
+
+    console.log("State: ", state)
+
+    if ( state.error !== "" ) {
+        return (
+            <div >
+                <h1 className="text-2xl">Register</h1>
+                <p className="text-red-500">{state.error}</p> 
             </div>
+        )
+    }
+
+    return (
+        <div>
+            <h1>Register</h1>
             <div>
-                <label htmlFor="email">Email: </label>
-                <input
-                    className={STYLE}
-                    name="email"
-                    type="email" maxLength={20} required />
+                <form action={action}>
+                    <div>
+                        <label htmlFor="name">Name: </label>
+                        <input
+                            className={STYLE}
+                            name="name"
+                            type="text" maxLength={20} required />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            className={STYLE}
+                            name="email"
+                            type="email" maxLength={20} required />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password: </label>
+                        <input
+                            className={STYLE}
+                            name="password"
+                            type="password" maxLength={20} required />
+                    </div>
+                    <div>
+                        <button className={STYLE} type="submit">Register</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input
-                    className={STYLE}
-                    name="password"
-                    type="password" maxLength={20} required />
-            </div>
-            <div>
-                <button className={STYLE} type="submit">Register</button>
-            </div>
-        </form>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
